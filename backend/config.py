bb962182689a99967792197f7c8bc7080f01852b
ArooSB <arooshasb@gmail.com>
2024-11-24 17:27:12 +0100

    fix the config to resolve the issue with public hosting

--- backend/config.py
+++ backend/config.py
@@ -16,7 +16,11 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/crm_backend_test')
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/crm_backend')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/crm_backend')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'postgresql://aroo:7Axx8cWyMQTxWj3TE642pLnVo8VZlKOO@dpg-csuvp223esus73cogp10-a.oregon-postgres.render.com/crm_db_yz92'
+    )
 
 
 config = {
